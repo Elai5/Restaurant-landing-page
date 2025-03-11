840fdb6a339d7a672103f879c51d453b9880608b
Elai5 <yvettelaine007@gmail.com>
2025-03-11 10:54:54 +0300

    added homepage text and image centered

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -14,6 +14,21 @@ const Home = () => {
         <div className="home-bannerImage-container">
           <img src={BannerBackground} alt="" />
         </div>
+        <div className="home-text-section">
+          <h1 className="primary-heading">
+            Your Favorite Food Delivered Hot & Fresh
+          </h1>
+          <p className="primary-text">
+            Healthy switcher chefs do all the prep wrok, like peeding, chooping
+            & marrinating, so you can cook fresh food.
+          </p>
+          <button className="secondary-button">
+            Order Now <FiArrowRight />
+          </button>
+        </div>
+        <div className="home-image-container">
+          <img src={BannerImage} alt="" />
+        </div>
       </div>
     </div>
   );
