7325e7a040bfcc9bcac8dd92eb3d76269bd6524a
Elai5 <yvettelaine007@gmail.com>
2025-03-11 08:55:07 +0300

    added imports from mui.com and react icons

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,26 @@
 /** @format */
 
 import React from "react";
+import Logo from "../Assets/Logo.svg";
+import { BsCart2 } from "react-icons/bs";
+import { HiOutlineBars3 } from "react-icons/hi2";
+import {
+  Box,
+  Drawer,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import commentRoundedIcon from "@mui/icons-matwrial/CommentRounded";
+import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
 
 const Navbar = () => {
+
+    
   return <div>Navbar</div>;
 };
 
