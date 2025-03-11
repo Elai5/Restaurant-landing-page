30c3b11be42a9ba28dc4fb18e5a0c676276ca2a7
Elai5 <yvettelaine007@gmail.com>
2025-03-11 12:15:19 +0300

    changed subtext for work

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -28,7 +28,7 @@ const Work = () => {
     <div className="work-section-wrapper">
       <div className="work-section-top">
         <p className="primary-subheading">Work</p>
-        <h1 className="primary-subheading">How It Works</h1>
+        <h1 className="primary-heading">How It Works</h1>
         <p className="primary-text">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum ut
           perferendis quisquam itaque totam sit alias sint?
